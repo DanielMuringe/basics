72b654576ccc1cb9df508377afffb94ee0e02c61
[Daniel Muringe] <[danielndungumuringe@gmail.com]>
2022-06-15 13:38:46 +0300

    Not much

---
+++ transverse.py
@@ -0,0 +1,24 @@
+def first(i):
+    for c in i:
+        eval(f"from {c[0]} import {c[1]}")
+
+s = 
+
+def recurse(recursable):
+
+    main_list = []
+    main_var = ''
+
+    def main(many):
+        is_list  = lambda x: isinstance(x, list)
+        is_dict  = lambda x: isinstance(x, dict)
+        is_tuple = lambda x: isinstance(x, tuple)
+        is_many  = lambda y: is_list(y) and is_tuple(y) and is_dict(y)
+
+        if is_many(many):
+            index = many
+            main_var.append(index)
+        
+        
+        
+    
