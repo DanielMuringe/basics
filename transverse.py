ab6ed0785f817c2e94fd163c945614825341d3ba
[Daniel Muringe] <[danielndungumuringe@gmail.com]>
2022-06-15 14:14:37 +0300

    almost solved the recursion problem

--- transverse.py
+++ transverse.py
@@ -10,9 +10,9 @@ def recurse(recursable):
     main_var = ''
 
     def main(many):
-        is_list  = lambda x: isinstance(x, list)
-        is_dict  = lambda x: isinstance(x, dict)
-        is_tuple = lambda x: isinstance(x, tuple)
+    is_list  = lambda x: isinstance(x, list)
+    is_dict  = lambda x: isinstance(x, dict)
+    is_tuple = lambda x: isinstance(x, tuple)
         is_many  = lambda y: is_list(y) and is_tuple(y) and is_dict(y)
 
         if is_many(many):
