62faa9ad2751f23cc663e21ea3b6991cfe312c0e
[Daniel Muringe] <[danielndungumuringe@gmail.com]>
2022-06-15 14:19:18 +0300

    Trying to see hoew branching works

--- transverse.py
+++ transverse.py
@@ -1,24 +1,33 @@
-def first(i):
-    for c in i:
-        eval(f"from {c[0]} import {c[1]}")
 
-s = 
 
+mine = [
+'dep0,parent0,item0',
+    ['dep1,parent0,item0','dep1,parent2,item1'],
+    ['dep1,parent0'],
+    ['dep1,parent0']
+]
 def recurse(recursable):
 
     main_list = []
-    main_var = ''
 
-    def main(many):
     is_list  = lambda x: isinstance(x, list)
     is_dict  = lambda x: isinstance(x, dict)
     is_tuple = lambda x: isinstance(x, tuple)
-        is_many  = lambda y: is_list(y) and is_tuple(y) and is_dict(y)
+    is_many  = lambda y: is_list(y) or is_tuple(y) or is_dict(y)
+
+    def main(many,depth=0):            
 
         if is_many(many):
-            index = many
-            main_var.append(index)
-        
-        
-        
-    
+            depth += 1
+            for item in many:
+                if is_many(item):
+                    main(item)
+                else:
+                    main_list.append(f'{depth}  {item}')
+        else:
+            main_list.append(many)
+        depth -= 1
+    main(recursable)
+    return main_list
+            
+print(recurse(mine))      
